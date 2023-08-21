da5585a4c22d640c6091850827af16f2095c35ea
aguilerad916 <aguilerad916@gmail.com>
2023-08-20 23:10:41 -0700

    fixed hydration error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gradient-to-r from-neutral-300 to-stone-400 py-4">
       <Head>
         <title>WeatherWise</title>
       </Head>
-      <body className="bg-gradient-to-r from-neutral-300 to-stone-400">
-        <main className="mt-5 mx-5">
-          <h1 className="flex justify-center text-xl font-medium mb-4">
-            Weatherwise
-          </h1>
-          <form>
-            <h2 className="flex justify-center text-lg mb-4">
-              Search for local weather
-            </h2>
-            <div className="flex justify-center mb-4">
-              <SearchBox />
-            </div>
-          </form>
-        </main>
-      </body>
-    </>
+      <main className="mt-5 mx-5">
+        <h1 className="flex justify-center text-xl font-medium mb-4">
+          Weatherwise
+        </h1>
+        <form>
+          <h2 className="flex justify-center text-lg mb-4">
+            Search for local weather
+          </h2>
+          <div className="flex justify-center mb-4">
+            <SearchBox />
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
